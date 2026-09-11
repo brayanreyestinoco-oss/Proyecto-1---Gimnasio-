059fb4720be4208886c81f4c9b52a315440aa952
grves12-1993 <grves12@gmail.com>
2026-09-10 22:15:50 -0600

    Error Clase Socio
    Gabriel

--- src/main/java/cr/ac/una/est/controlmembresiasgimnasio/modelo/planes/Socio.java
+++ src/main/java/cr/ac/una/est/controlmembresiasgimnasio/modelo/planes/Socio.java
@@ -7,12 +7,12 @@ public class Socio {
 
     private int idSocio;
     private String nombreSocio;
-    private LocalDate expiraMembrecia;
+    private LocalDate expiraMembresia;
 
     public Socio(int idSocio, String nombreSocio, LocalDate expiraMembrecia) {
         this.idSocio = idSocio;
         this.nombreSocio = nombreSocio;
-        this.expiraMembrecia = expiraMembrecia;
+        this.expiraMembresia = expiraMembrecia;
     }
     //Retorna el numero de identificacion del socio sin recibir parametros
     public int getIdSocio()
@@ -25,9 +25,9 @@ public class Socio {
         return nombreSocio;
     }
     //Retorna la Fecha de expiracion de la membrecia sin recibir parametros
-    public LocalDate getExpiraMembrecia() {
+    public LocalDate getExpiraMembresia() {
 
-        return expiraMembrecia;
+        return expiraMembresia;
     }
 }
 
--- src/main/java/cr/ac/una/est/controlmembresiasgimnasio/service/ControlAcceso.java
+++ src/main/java/cr/ac/una/est/controlmembresiasgimnasio/service/ControlAcceso.java
@@ -1,16 +1,16 @@
 package cr.ac.una.est.controlmembresiasgimnasio.service;
 
-import main.java.cr.ac.una.est.controlmembresiasgimnasio.modelo.planes.Socio;
+import cr.ac.una.est.controlmembresiasgimnasio.modelo.planes.Socio;
 import java.util.ArrayList;
 import java.time.LocalDate;
 
-import static jdk.jfr.internal.util.Utils.isBefore;
 
 public class ControlAcceso {
 
     private ArrayList<Socio> socios;
 
     public ControlAcceso(ArrayList<Socio> socios) {
+
         this.socios = socios;
     }
 
@@ -37,7 +37,7 @@ public class ControlAcceso {
         }
         LocalDate hoy = LocalDate.now();
 
-        return !socio.getExpiraMembrecia() isBefore(hoy);
+        return !socio.getExpiraMembresia().isBefore(hoy);
     }
 
 }
