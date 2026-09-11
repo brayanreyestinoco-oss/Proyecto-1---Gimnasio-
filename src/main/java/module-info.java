d7c3466a769c0de27c01c81441cc635877773b57
brayanreyestinoco-oss <brayan.reyes.tinoco@est.una.ac.cr>
2026-09-10 19:34:42 -0600

    Menu Principal

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module cr.ac.una.est.controlmembresiasgimnasio {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.dlsc.fxmlkit;
 
     opens cr.ac.una.est.controlmembresiasgimnasio to javafx.fxml;
     opens cr.ac.una.est.controlmembresiasgimnasio.vista to javafx.fxml, javafx.base;
